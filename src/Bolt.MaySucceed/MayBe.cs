c1ed51b19e8d698791435fd15be7e9ebfa734ad9
ruhul <ruhul.amin@carsales.com.au>
2024-03-14 19:33:15 +1100

    Fix has value

--- src/Bolt.MaySucceed/MayBe.cs
+++ src/Bolt.MaySucceed/MayBe.cs
@@ -7,21 +7,23 @@ public readonly struct MayBe<T>
     public MayBe(T? value)
     {
         IsNone = value is null;
+        HasValue = !IsNone;
         Value = value;
     }
 
     public MayBe()
     {
         IsNone = true;
+        HasValue = !IsNone;
     }
     
-    public T? Value { get; init; }
+    public T? Value { get; }
     
     [MemberNotNullWhen(false, nameof(Value))]
-    public bool IsNone { get; init; }
+    public bool IsNone { get; }
     
     [MemberNotNullWhen(true, nameof(Value))]
-    public bool HasValue { get; init; }
+    public bool HasValue { get; }
 
     public static implicit operator T?(MayBe<T> mayBe) => mayBe.Value;
     public static implicit operator MayBe<T>(T? value) => new(value);
