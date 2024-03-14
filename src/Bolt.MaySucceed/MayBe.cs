9d0ce2caa56f32867c9fb2b57f898405ee6ddf5b
ruhul <ruhul.amin@carsales.com.au>
2024-03-14 18:17:06 +1100

    Added a new struct to represent something which value can be null

---
+++ src/Bolt.MaySucceed/MayBe.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Bolt.MaySucceed;
+
+public readonly struct MayBe<T>
+{
+    public MayBe(T? value)
+    {
+        IsNone = value is null;
+        Value = value;
+    }
+
+    public MayBe()
+    {
+        IsNone = true;
+    }
+    
+    public T? Value { get; init; }
+    
+    [MemberNotNullWhen(false, nameof(Value))]
+    public bool IsNone { get; init; }
+
+    public static implicit operator T?(MayBe<T> mayBe) => mayBe.Value;
+    public static implicit operator MayBe<T>(T? value) => new(value);
+}
